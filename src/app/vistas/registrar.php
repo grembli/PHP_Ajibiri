eaef4a131f8e2efaebc492bea5d3973454202660
Tecpem <34627738+Tecpem@users.noreply.github.com>
2017-12-17 20:06:16 +0100

    Add files via upload

--- src/app/vistas/registrar.php
+++ src/app/vistas/registrar.php
@@ -2,25 +2,44 @@
 //Indicamos que la salida no vaya al cliiente sino que se guarde en un buffer
     ob_start(); 
 ?>
-
+<script>
+    $(document).ready(function(){
+ $('#email').change(function(){  
+                //AJAX
+                $.ajax({
+                    url:"<?=RUTA?>comprobar_email",
+                    type:"GET",
+                    data:"email="+$('#email').val(),
+                    success:function(respuesta){
+                        if(respuesta=="existe")
+                        {
+                            alert("Ya existe ese email");
+                        }
+                    },
+                    beforeSend:function(){
+                        $('#preloader').css('display','inline');
+                    },
+                    complete:function(){
+                        $('#preloader').css('display','none');
+                    }
+                });
+            });
+             });
+        </script>
 <form action="<?=RUTA?>registrar" method="post" enctype="multipart/form-data" id="formulario_registrar">
                     
                 <input type="email" name="email" placeholder="Email" required class="border" id="email">
                 <input type="password" name="password" placeholder="Contraseña" required class="border">
-                <input type="text" name="nombre" placeholder="Nombre" required class="border">
+                <input type="text" name="nombre" placeholder="Nombre" class="border">
                 <input type="text" name="apellidos" placeholder="Apellidos" required class="border">
                <input type="text" name="provincia" placeholder="Provincia" required class="border">
-               <!-- <select name="provincia" id="provincia" class="border">
-                <option></option>
-                <?php foreach ($provincias as $provincia):?>
-                    <option value="<?php echo $provincia->getId()?>"><?php echo $provincia->getNombre()?></option>
-                <?php endforeach; ?>
-                </select>-->
+              
                 <input type="text" name="telefono" placeholder="Telefono" class="border">
                 <input type="file" name="foto">
                 <input type="submit" value="Insertar" class="boton_submit">
                       
             </form>
+        <img src="<?=RUTA?>web/imagenes/30.gif" height="20" id="preloader" style="display: none">
 
 <?php 
 //Obtenemos el contenido del buffer 
