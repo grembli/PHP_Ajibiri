a82df14a89f33723c6bd02f7272a97cad08373e4
Jose <grembliluf@gmail.com>
2017-12-18 19:22:08 +0100

    Añadidas vistas insertar y editar(sin hacer)
    jarl

--- src/app/vistas/insertar_anuncio.php
+++ src/app/vistas/insertar_anuncio.php
@@ -1,8 +1,34 @@
-<?php
+<?php ob_start(); ?>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<script src="<?= RUTA ?>web/js/jQuery-TE_v.1.4.0/jquery-te-1.4.0.min.js" type="text/javascript"></script>
+<link href="<?= RUTA ?>web/js/jQuery-TE_v.1.4.0/jquery-te-1.4.0.css" type="text/css" rel="stylesheet">
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('textarea').jqte();
+    });
+</script>
+<div id="insertar">
+    <form id="formulario_insertar_mensaje" action="<?= RUTA ?>insertar_anuncio" method="post">
+    <div id="fotos" class="col-sm-6">
+        <input type="file" name="foto0">
+        <input type="file" name="foto1">
+        <input type="file" name="foto2">
+        <input type="file" name="foto3">
+        
+    </div>
+    <div id="formulario" class="col-sm-6">
+   
+        <input type="text" name="titulo" id="titulo_mensaje" placeholder="Titulo..." style="width: 80%">
+        <textarea rows="15" name="texto" id="texto_mensaje" placeholder="Texto del mensaje..."></textarea>
+        <input type="submit" value="Insertar">
+    </div>
+    </form>
+
+</div>
+<?php
+$contenido_vista = ob_get_clean();
+
+require '../app/plantillas/plantilla_bootstrap.php';
+?>
+
 
