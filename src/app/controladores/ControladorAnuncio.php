d3a49cf0ea4aee6da31158d5c7e1aaf4cda57a20
Jose <grembliluf@gmail.com>
2017-12-19 23:26:16 +0100

    Controlador Usuario bien hecho
    Jarl

--- src/app/controladores/ControladorAnuncio.php
+++ src/app/controladores/ControladorAnuncio.php
@@ -12,8 +12,9 @@
  * @author Usuario1
  */
 class ControladorAnuncio {
-    public function insertar(){
-        if($_SERVER['REQUEST_METHOD']=='POST'){
+
+    public function insertar() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             //Insertar el mensaje en la DB
             $anuncio = new Anuncio();
             $anuncio->setTitulo($_POST['titulo']);
@@ -21,71 +22,67 @@ class ControladorAnuncio {
             $anuncio->setPrecio($_POST['precio']);
             $anuncio->setUsuarios_id(Sesion::obtener()->getId());
             $anuncio->setFotos($_POST['fotos']);
-            
-            if($anuncio->insertar()){
+
+            if ($anuncio->insertar()) {
                 Utils::guardar_mensaje("Anuncio Creado");
-            }
-            else{
+            } else {
                 Utils::guardar_mensaje("Error al insertar anuncio");
             }
-            header('location:'.RUTA.'listar_anuncios');
-        }
-        else{
+            header('location:' . RUTA . 'listar_anuncios');
+        } else {
             require '../app/vistas/insertar_anuncio.php';
         }
     }
-    public function borrar(){
-        if($_SERVER['REQUEST_METHOD']=='POST'){
-            
+
+    public function borrar() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
             $anuncio = new Anuncio();
             $anuncio->obtener($_POST['id']);
             print_r($anuncio);
             die();
-            if($anuncio->borrar()){
-                header('location:'.RUTA);
-            }
-            else{
+            if ($anuncio->borrar()) {
+                header('location:' . RUTA);
+            } else {
                 Utils::guardar_mensaje("No se ha podido eliminar");
-                header('location:'.RUTA);
+                header('location:' . RUTA);
             }
-            
         }
     }
-    public function editar(){
-        if($_SERVER['REQUEST_METHOD']=='POST'){
-            
+
+    public function editar() {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
             $anuncio = new Anuncio();
             $anuncio->setTitulo($_POST['titulo']);
             $anuncio->setTexto($_POST['texto']);
             $anuncio->setId_usuario(Sesion::obtener());
-            
-            if($anuncio->actualizar()){
+
+            if ($anuncio->actualizar()) {
                 Utils::guardar_mensaje("Anuncio Editado");
-            }
-            else{
+            } else {
                 Utils::guardar_mensaje("Error al editar anuncio");
             }
-            header('location:'.RUTA.'listar_anuncios');
-            
+            header('location:' . RUTA . 'listar_anuncios');
         }
     }
-    public function listar(){
+
+    public function listar() {
         //Obtener todos los mensajes
         $anuncios = Anuncio::obtener_todos();
-        
+
         //Inculir la vista 
         require '../app/vistas/listar_anuncios.php';
-        
     }
-    public function ver(){
+
+    public function ver() {
         $anuncio = new Anuncio();
-        $usuario = new Usuario();
-        $id = filter_var($_GET['parametro1'],FILTER_SANITIZE_NUMBER_INT);
+
+        $id = filter_var($_GET['parametro1'], FILTER_SANITIZE_NUMBER_INT);
         $anuncio->obtener($id);
-        if(Sesion::existe()){
-            $usuario->obtener($anuncio->getUsuarios_id());
-        }
+        $anuncio->cargar_usuario();
+        
         require '../app/vistas/ver_anuncio.php';
     }
-}
 
+}
