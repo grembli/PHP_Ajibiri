155fc83e5a3167f9b62ebd4d1754e5f75814b78b
Tecpem <34627738+Tecpem@users.noreply.github.com>
2017-12-19 22:52:23 +0100

    Add files via upload

--- src/app/vistas/registrar.php
+++ src/app/vistas/registrar.php
@@ -2,44 +2,36 @@
 //Indicamos que la salida no vaya al cliiente sino que se guarde en un buffer
     ob_start(); 
 ?>
-<script>
-    $(document).ready(function(){
- $('#email').change(function(){  
-                //AJAX
-                $.ajax({
-                    url:"<?=RUTA?>comprobar_email",
-                    type:"GET",
-                    data:"email="+$('#email').val(),
-                    success:function(respuesta){
-                        if(respuesta=="existe")
-                        {
-                            alert("Ya existe ese email");
-                        }
-                    },
-                    beforeSend:function(){
-                        $('#preloader').css('display','inline');
-                    },
-                    complete:function(){
-                        $('#preloader').css('display','none');
-                    }
-                });
-            });
-             });
-        </script>
+
+        
+        <h3>Registrar</h3>
+        
 <form action="<?=RUTA?>registrar" method="post" enctype="multipart/form-data" id="formulario_registrar">
-                    
+                   <div class="separador3"></div> 
                 <input type="email" name="email" placeholder="Email" required class="border" id="email">
                 <input type="password" name="password" placeholder="Contraseña" required class="border">
                 <input type="text" name="nombre" placeholder="Nombre" class="border">
                 <input type="text" name="apellidos" placeholder="Apellidos" required class="border">
-               <input type="text" name="provincia" placeholder="Provincia" required class="border">
+                <select name="provincia" id="provincia" class="formulario_index" style="color:black">
+                            <option></option>
+                            <?php $provincias= Utils::obtener_provincias(); ?>
+                            <?php foreach ($provincias as $provincia): ?>
+                                <option value="<?php echo $provincia['id_provincia'] ?>" style="color:black"><?php echo $provincia['provincia'] ?></option>
+                <?php endforeach; ?>
+                        </select>
+               
               
                 <input type="text" name="telefono" placeholder="Telefono" class="border">
                 <input type="file" name="foto">
+                <br>
+                <br>
                 <input type="submit" value="Insertar" class="boton_submit">
+                
+                <img src="<?=RUTA?>web/imagenes/30.gif" height="20" id="preloader" style="display: none">
                       
             </form>
-        <img src="<?=RUTA?>web/imagenes/30.gif" height="20" id="preloader" style="display: none">
+        <p id="texto_registro">Al registrarte, aceptas las Condiciones de Servicio y la Política de Privacidad. Otros podrán encontrarte por correo electrónico o por número de teléfono cuando sea proporcionado.</p>
+        
 
 <?php 
 //Obtenemos el contenido del buffer 
--- src/app/vistas/ver_anuncio.php
+++ src/app/vistas/ver_anuncio.php
@@ -1,8 +1,99 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ob_start(); 
+?>
 
+<div id="anuncio">
+    <?php if(Sesion::existe()/* && Sesion::obtener()->getId() == $anuncio->getUsuario_id()*/):?>
+    <div id="iconos">
+      
+        <a href="<?=RUTA?>editar_anuncio/<?= $anuncio->getId() ?>"><span class="glyphicon glyphicon-pencil"></span> </a>
+         <a href="<?=RUTA?>borrar_anuncio/<?= $anuncio->getId() ?>"><span class="glyphicon glyphicon-remove"></span> </a>
+    </div>
+    <?php endif;?>
+    
+    <div>
+        <?php $fotos = $anuncio->getFotos(); ?>
+        <?php if($fotos == null):?>
+            <h3><?=$anuncio->getTitulo()?></h3>
+            <div class="separador3"></div>
+            <div>
+                <p><?=$anuncio->getDescripcion()?></p>
+                <p><?=$anuncio->getPrecio(); echo "€"?></p>
+            </div>
+            <?php if(Sesion::existe()):?>
+            <div>
+        
+            </div>
+            <?php else:?>
+            <div>
+                <br>
+                <br>
+                <h4>Debes estar registrado para poder ver mas información</h4>
+            </div>
+            <?php endif;?>
+               
+            
+        <?php else:?>
+          <h3><?=$anuncio->getTitulo()?></h3>
+          <div class="separador3"></div>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner">
+                    <?php foreach ($fotos as $foto): ?>
+                        <div class="item <?php
+                        if ($foto->getPrincipal() == 1) {
+                            echo "active";
+                        }
+                        ?>">
+                             
+                            <img src="<?= RUTA ?>web/imagenes/fotos/<?= $foto->getNombre(); ?>" class="img-responsive" style="width:100%" alt="Image">
+                           
+                            <br>
+                            
+                            <br>
+                        </div>
+           <?php endforeach; ?>
+                </div>
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+        </div>
+          
+        <div>
+            <p><?=$anuncio->getDescripcion()?></p>
+            <p><?=$anuncio->getPrecio(); echo "€"?></p>
+        </div>
+        <?php if(Sesion::existe()):?>
+            <div>
+        
+            </div>
+        <?php else:?>
+            <div>
+                <br>
+                <br>
+                <h4>Debes estar registrado para poder ver mas información</h4>
+            </div>
+        <?php endif;?>
+        
+        <?php endif;?>
+   
+</div>
+
+
+
+
+<?php
+//Obtenemos el contenido del buffer y lo guardamos en $contenido_vista
+$contenido_vista = ob_get_clean(); 
+
+//Incluimos la plantilla
+require '../app/plantillas/plantilla_bootstrap.php';
+
+?>
